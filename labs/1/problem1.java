c5d3c773dc360578c20491f6fae8f6df3fa8b983
Juan Arias <juan.arias@marist.edu>
2019-02-04 11:21:43 -0500

    Feedback

--- labs/1/problem1.java
+++ labs/1/problem1.java
@@ -7,7 +7,7 @@ public class problem1 {
 	public static void main(String[] args) {
 		double problem ;
 		
-		problem = (15.4*6.5)-(4.3*3.0) / (46.3-3.5);
+		problem = ((15.4*6.5)-(4.3*3.0)) / (46.3-3.5); // JA
 		
 		System.out.println("Answer is " + problem);
 	}
--- labs/1/problem2.java
+++ labs/1/problem2.java
@@ -8,7 +8,7 @@ public class problem2 {
 		double pie1;
 		double pie2;
 		
-		pie1 = 4*(1.0-(1/3)+(1/5)-(1/7)+(1/9)-(1/11));
+		pie1 = 4*(1.0-(1./3)+(1./5)-(1./7)+(1./9)-(1./11)); // JA
 		pie2 = 4.0*(1.0-(1.0/3.0)+(1.0/5.0)-(1.0/7.0)+(1.0/9.0)-(1.0/11.0)+(1.0/13.0));
 		
 		System.out.println("The answer to pie of first equation is " + pie1);
